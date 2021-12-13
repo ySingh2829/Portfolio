bd1644df7b9885a554b68e555940f97a80d7b14f
Yash Singh <yashsi3007@gmail.com>
2021-12-14 00:51:50 +0530

    add about me section

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 let nameYash = document.getElementById('name-yash');
 
 nameYash.onclick = function (e) {
-    e.target.style.color = 'brown';
+    if (e.target.style.color === '') {
+        e.target.style.color = 'brown';
+    } else {
+        e.target.style.color = '';
+    }
+};
+
+nameYash.onmouseenter = function (e) {
+    e.target.style.cursor = 'pointer';
+    e.target.style.textDecoration = 'underline';
+};
+
+nameYash.onmouseout = function (e) {
+    e.target.style.textDecoration = '';
 };
